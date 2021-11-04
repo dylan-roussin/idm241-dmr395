dacd9ceaa8f8c6404cad2277903537d05e0704c1
Dylan <71848660+dylan-roussin@users.noreply.github.com>
2021-11-04 06:51:03 -0400

    type fix

--- beta/build/script.js
+++ beta/build/script.js
@@ -1,5 +1,11 @@
 function clickFunction()
-{
+  {
     const element = document.getElementById('sub');
     element.classList.toggle("sub-click");
+
+    if (element.innerHTML === "SUBSCRIBE") {
+      element.innerHTML = "SUBSCRIBED";
+    } else {
+      element.innerHTML = "SUBSCRIBE";
+    }
 }
